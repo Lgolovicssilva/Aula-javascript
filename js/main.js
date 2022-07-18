72e4328145eedadcffb528bf5f2acc50f967ea16
lgolovics <lgolovicssilva1@gmail.com>
2022-07-18 15:28:16 -0300

    versão final introducao ao javascript

---
+++ js/main.js
@@ -0,0 +1,118 @@
+//function redirecionar(){
+//	window.open("https://www.linkedin.com/in/lucas-gabriel-olovics-da-silva-058b79188/"); //Outra aba
+//	window.location.href = "https://www.linkedin.com/in/lucas-gabriel-olovics-da-silva-058b79188/"; // Mesma aba
+//}
+//function trocar(elemento){
+//	document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+//	alert("trocar texto");
+//	elemento.innerHTML = "Obrigado por passar o mouse";
+//}
+//function voltar(elemento){
+//	document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+//	elemento.innerHTML = "Passe o mouse aqui";
+//}
+//function clicou(){
+//	document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+//}
+//	console.log(getElementByid('agradecimento'));
+//	alert("Obrigado por clicar!");
+//function load() {
+//	alert("Página carregada");
+//}
+//function funcaoChange(elemento){
+//	console.log(elemento.value);
+//}
+
+
+
+
+
+//function validaIdade(idade){
+//	var validar;
+//	if (idade >= 18){
+//		validar = true;
+//	}else{
+//		validar = false;
+//	}
+//	return validar;
+//}
+//var idade = prompt("Qual é sua idade?");
+//console.log(validaIdade(idade));
+//function soma(n1, n2){
+//	return n1 + n2;
+//}
+//function setReplace(frase, nome, novo_nome){
+//	return frase.replace(nome, novo_nome);
+//}
+//alert(soma(5, 10));
+//alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+
+
+//var d = new Date();
+//alert(d);
+//alert(d.getMounth()+1);
+//alert(d.getDay());
+//alert(d.getHours());
+
+
+
+//var count;
+//for(count=0; count <= 5; count++){
+//	alert(count);
+//}
+//var count = 0;
+//while (count <=5);{
+//	console.log(count);
+//	count = count + 1;
+//	alert(count);
+//	//count++;
+//}
+//var idade = prompt("Qual sua idade?");
+//var idade = 18;
+//if (idade >= 18) {
+//	alert("maior de idade");
+//}else{
+//	alert("menor de idade");
+//}
+
+
+
+
+
+
+//var frutas = [{nome:"Maça", cor:"Vermelha"}, {nome:"Uva", cor:"Roxa"}]
+//console.log(fruta.nome);
+//console.log(fruta.cor);
+//console.log(frutas);
+//alert(frutas[1].nome);
+//var lista = ["Maça", "Pera", "Laranja"];
+//lista.push("Uva"); //para colocar
+//lista.pop("Uva"); // para tirar
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+//console.log(lista.length);
+//console.log(lista.reverse());
+
+
+
+
+
+
+
+
+//var nome = "Lucas Olovics";
+//var idade = 22;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//var n1 = 5;
+//var n2 = 3;
+//alert("Bem vindo, " + nome);
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toLowerCase());
+//console.log(n1 * n2);
+//alert(frase.replace("Japão", "Brasil"));
